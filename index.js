590cadab411ad42d24a2863da8246ea1136e656c
BenjaminJacobson <ben.jacobson@flatironschool.com>
2023-05-07 15:23:59 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(act='roller-skate'){
+    return `This Saturday, I want to ${act}!`
+}
+
+function mondayWork(action="go to the office"){
+    return `This Monday, I will ${action}.`
+}
+
+
+function wrapAdjective(arg1 = '*'){
+    return function(arg2 ='special'){
+        return `You are ${arg1}${arg2}${arg1}!`
+    }
+}
